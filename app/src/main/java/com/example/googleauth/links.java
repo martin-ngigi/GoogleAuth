b3011442035b4c978592029d6c1d979ca6b9a285
MartinNgigi <martinwainaina001@gmail.com>
2022-07-06 12:41:44 +0300

    Added instructions on getting sha1

--- app/src/main/java/com/example/googleauth/links.java
+++ app/src/main/java/com/example/googleauth/links.java
@@ -2,6 +2,10 @@ package com.example.googleauth;
 
 public class links {
     //https://developers.google.com/identity/sign-in/android/start-integrating
+    //to get SHA1 in android studio, just next to build icon, i.e on the right side of the build icon, click the drop down menu and select "GoogleAuth[signingreport]"
     //generating sha1 - > https://intensecoder.com/generate-sha1-key-for-new-android-studio-4-2-and-newer/
     //sha1 url https://console.cloud.google.com/apis/credentials?authuser=0&project=auth-ece0b
+
+
+
 }
